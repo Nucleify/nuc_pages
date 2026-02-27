4f2f225d23474ae0e0e51d0e2d2d5e888c4d1cb2
SzymCode <szymon.radomski@yahoo.com>
2026-02-27 19:08:20 +0100

    chore: remove deprecated PagesController

--- nuc_pages.php
+++ nuc_pages.php
@@ -6,15 +6,6 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 
 class nuc_pages extends ServiceProvider
 {
-    /**
-     * The path to your application's "home" route.
-     *
-     * Typically, users are redirected here after authentication.
-     *
-     * @var string
-     */
-    public const HOME = '/dashboard';
-
     public function boot(): void
     {
         app()->register(App\Providers\RouteServiceProvider::class);
